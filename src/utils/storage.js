a5485f4e46f6d755b84b4e32c3c547c760938d52
lexuantien <tienlx97@gmail.com>
2021-08-23 23:21:57 +0700

    remove SHighlight

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,20 +1,20 @@
 export const SetLS = (key, value) => {
   window.localStorage.setItem(key, value);
-}
+};
 
-export const GetLS = key => {
+export const GetLS = (key) => {
   return window.localStorage.getItem(key);
-}
+};
 
-export const  CheckLS = key => {
+export const CheckLS = (key) => {
   return window.localStorage.getItem(key) ? true : false;
-}
+};
 
 const ThemeLSVal = () => {
-  if (!CheckLS('tlog-theme')) {
-    SetLS("tlog-theme", "light");
+  if (!CheckLS("tlog-theme")) {
+    SetLS("tlog-theme", "dark");
   }
-  return GetLS('tlog-theme');
-}
+  return GetLS("tlog-theme");
+};
 
 export default ThemeLSVal;
